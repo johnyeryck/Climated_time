6a768648579103d45a8cc594a51f8338145bdfbb
Johnyeryck <you@example.com>
2025-02-26 22:44:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <main className='h-44 w-48 ml-auto mr-auto mt-56 bg-violet-400 rounded-xl'>
+      <p></p>
+    </main>
+    </>
+  )
+}
+
+export default App
