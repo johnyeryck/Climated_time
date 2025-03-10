6ce7ba7d72f2d21743ed8b555fbceab3e2d1c07b
Johnyeryck <you@example.com>
2025-03-10 14:36:56 -0300

    finished, I guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,74 @@
-import React, {useState} from 'react'
+import React, { useState} from 'react'
 import { IWearth } from './types/typeweather'
-import Lupa from './img/lupa.png'
 import weather from './services/api/weather'
+import { ObjImg } from './types/typeweather'
+import Nfound from './img/imgweather/no-results.png'
 function App() {
   const [search, setSearch] = useState<string>('')
   const [weatherData , setWeatherData] = useState<IWearth[]>([]) 
-  
   // Input submit //
   const handleFetch = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     
       const data = await weather(search).getWeather()
-      
       setWeatherData([data])
-  }
-  
-  return (
-    <>
-      <main className='h-64 w-56 ml-auto mr-auto mt-56 bg-violet-400 rounded-xl'>
-        <form className='flex' onSubmit= {handleFetch}>
+    }
+
+
+    return (
+      <>
+      <main className='h-auto  w-60 ml-auto mr-auto mt-56  rounded-xl  bg-violet-500  hover:shadow-xl hover:shadow-purple-700 '>
+        <form className='flex ' onSubmit= {handleFetch}>
+          
           <input
-            className='border rounded-md mt-2 ml-3 shadow-xl '
+            autoComplete='on'
+            list='browsers'
+            className='border rounded-md mt-2 ml-3 shadow-xl mb-6 dark:bg-gray-800 text-white border[#8707ff] hover:border-violet-600 px-2  '
             type='search'
-            placeholder='  Cidade'
+            placeholder='Local'
             onChange={(e: React.ChangeEvent<HTMLInputElement>)=> setSearch(e.currentTarget.value) }
             required
+            
           />
-          <button className='size-5 mt-3 absolute ml-44' type='submit'><img src={Lupa} alt='Search' /></button>
+        
+          <button className='size-5 mt-3 absolute ml-44' type='submit' ></button>
         </form>
-         
+        {weatherData.length >0 && weatherData[0].cod != 200 ? (
+          <div >
+            <img src={Nfound} className='size-20 ml-auto mr-auto'/>
+            <p className='text-center'>Local não encontrado</p>
+          </div>
+        ): null}
         {weatherData && weatherData.length > 0 && weatherData[0].main ? (
           weatherData.map((data) => (
-            <div key={data.id} className='mt-4 '>
-                <h1 className='text-center text-2xl text-black font-extrabold' >{data.name},{data.sys?.country}</h1>
-                <h2 className='text-center text-xl text-black'>{(data.main.temp - 273.15).toFixed(1)}°C</h2>
-              <div className='flex mt-5 '>
-                <p className='text-black mr-auto ml-auto' >Min:<br/>{(data.main.temp_min - 273.15).toFixed(1) }</p>
-                <p className='text-black ml-auto mr-auto'>Max:<br/>{(data.main.temp_max - 273.15).toFixed(1) }</p> 
+            <section key={data.id} className='mb-48 '>
+              <div className='bg-black text-white  rounded-2xl h-auto w-48 ml-auto mr-auto bg'>
+              {  Object.entries(ObjImg).map(([key])=>
+              key === data.weather[0].description ? (
+                <img src={ObjImg[key] } key={key} className='ml-auto mr-auto size-20'
+                
+                /> 
+                
+              ) : null
+            )}  
+                <h1 className='text-center text-2xl font-extrabold '  >{data.name},{data.sys?.country}</h1>
+                <h2 className='text-center text-xl '>{(data.main.temp - 273.15).toFixed(1)}°C</h2>
               </div>
-            </div>)
+              <footer className=' flex mt-4'>
+                <div className='w-full  '>
+                    <p className='text-black  ml-10' >Min:<br/>{(data.main.temp_min - 273.15).toFixed(1) }</p>
+                </div> 
+                  <div className='w-full '> 
+                      <p className='text-black  ml-10 '>Max:<br/>{(data.main.temp_max - 273.15).toFixed(1) }</p> 
+                  </div>
+              </footer>
+            </section>)
           ))
-         : (
-          <p className='text-center mt-4'>Busque a localização</p>
-          )}
+         : weatherData.length == 0 ?(
+           <p className='text-center mt-4'>Busque a localização</p>
+
+         ): null
+          }
       </main>
     </>
   )
--- src/types/typeweather.tsx
+++ src/types/typeweather.tsx
@@ -1,3 +1,13 @@
+import Sun from '../img/imgweather/01d.png'
+import Neve from '../img/imgweather/13d.png'
+import few from '../img/imgweather/02d.png'
+import scattered from '../img/imgweather/03d@2x.png'
+import Chuva from '../img/imgweather/10d.png'
+import Broken from '../img/imgweather/04d.png'
+import Shower from '../img/imgweather/09d@2x.png'
+import Mist from '../img/imgweather/50d@2x.png'
+import Thunder from '../img/imgweather/11d@2x.png'
+import Overclast from '../img/imgweather/cloudy.png'
 export interface IWearth{
     id: number
     name : string
@@ -7,6 +17,7 @@ export interface IWearth{
       temp_min : number
       temp_max : number
     }
+    cod : number
     sys :{
       country : string
     }
@@ -14,4 +25,18 @@ export interface IWearth{
       main: string
       description: string
     }
+  }
+
+
+  export const ObjImg : { [key: string]: any }= {
+    'clear sky' : Sun,
+    'few clouds' : few,
+    'scattered clouds' : scattered,
+    'broken clouds' : Broken,
+    'shower rain' : Shower,
+    'rain' : Chuva,
+    'thunderstorm' : Thunder,
+    'snow' : Neve,
+    'mist' : Mist,
+    'overcast clouds' : Overclast
   }
